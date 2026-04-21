8a0ec9292d9542a903e7e53c297f8dfc2bc33593
yinzefeng.yzf <yinzefeng.yzf@alibaba-inc.com>
2026-04-22 01:15:36 +0800

    upd

--- src/ailego/buffer/vector_page_table.cc
+++ src/ailego/buffer/vector_page_table.cc
@@ -276,7 +276,7 @@ char *VecBufferPool::acquire_buffer(block_id_t block_id, size_t offset,
     return nullptr;
   }
   buffer = mmap_addr_ + offset;
-  mlock(buffer, size);
+  mlock2(buffer, size, MLOCK_ONFAULT);
   madvise(buffer, size, MADV_WILLNEED);
   // memcpy(buffer, mmap_addr_ + offset, size);
   return page_table_.set_block_acquired(block_id, buffer, size);
